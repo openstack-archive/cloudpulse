2ece072753f9fc94fc0ee9600c91d8719ee6ab09
vinod pandarinathan <vpandari@cisco.com>
2015-06-12 14:46:39 -0700

    Cloudpulse application code
    Change-Id: I84795eae78ff3109af3e4ba525615382dc04f377
    Implements: blueprint cloudpulse-api-handler

---
+++ cloudpulse/api/app.py
@@ -0,0 +1,63 @@
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from oslo_config import cfg
+import pecan
+
+from cloudpulse.api import auth
+from cloudpulse.api import config as api_config
+from cloudpulse.api import middleware
+from cloudpulse.common.timerthread import cpulseTimer
+from cloudpulse.common.timerthread import timerfunc
+
+# Register options for the service
+API_SERVICE_OPTS = [
+    cfg.IntOpt('port',
+               default=9511,
+               help='The port for the cloudpulse API server'),
+    cfg.StrOpt('host',
+               default='127.0.0.1',
+               help='The listen IP for the cloudpulse API server'),
+    cfg.IntOpt('max_limit',
+               default=1000,
+               help='The maximum number of items returned in a single '
+                    'response from a collection resource.')
+]
+
+CONF = cfg.CONF
+opt_group = cfg.OptGroup(name='api',
+                         title='Options for the cloudpulse-api service')
+CONF.register_group(opt_group)
+CONF.register_opts(API_SERVICE_OPTS, opt_group)
+
+
+def get_pecan_config():
+    # Set up the pecan configuration
+    filename = api_config.__file__.replace('.pyc', '.py')
+    return pecan.configuration.conf_from_file(filename)
+
+
+def setup_app(config=None):
+    if not config:
+        config = get_pecan_config()
+
+    app_conf = dict(config.app)
+
+    app = pecan.make_app(
+        app_conf.pop('root'),
+        logging=getattr(config, 'logging', {}),
+        wrap_app=middleware.ParsableErrorMiddleware,
+        **app_conf
+    )
+
+    cpulseTimer(10, timerfunc, "Cpulse")
+    return auth.install(app, CONF, config.app.acl_public_routes)
