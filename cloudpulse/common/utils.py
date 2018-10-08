a7a65dd83b6a692ea6bec9610bc72b33a71bfeeb
zhulingjie <easyzlj@gmail.com>
2018-10-08 02:10:39 -0400

    Python 3 compatibility: fix has_key
    For Python 3.0, has_key() isn't supported already, it can be replaced with in.
    
    Change-Id: I0d24e13724a6b2bbd593e80b8f90b2ed5a6c8aa7

--- cloudpulse/common/utils.py
+++ cloudpulse/common/utils.py
@@ -348,7 +348,7 @@ def temporary_mutation(obj, **kwargs):
             do_something_that_needed_deleted_objects()
     """
     def is_dict_like(thing):
-        return hasattr(thing, 'has_key')
+        return isinstance(thing, dict)
 
     def get(thing, attr, default):
         if is_dict_like(thing):
